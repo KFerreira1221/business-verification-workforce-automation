83c4c3e276bd8b430bcfbe0724f85b59e411afcf
Joe Landers <jlanders2017@fau.edu>
2026-06-29 23:29:25 -0400

    Reports.jsx

---
+++ dashboard/pages/Reports.jsx
@@ -0,0 +1,12 @@
+function Reports() {
+    return (
+        <div>
+            <h1>Reports</h1>
+            <button>Generate Report</button>
+            <br /><br />
+            <buton>Download PDF</button>
+        </div>
+    );
+}
+
+export default Reports;
